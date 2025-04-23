683874af259c1f7d891660e253890e17504630c8
mohamed-295 <ma2267204@gmail.com>
2025-04-24 00:48:07 +0200

    adding reset

--- design3/design0/src/TestBench/register_b_tb.vhd
+++ design3/design0/src/TestBench/register_b_tb.vhd
@@ -6,7 +6,8 @@ end entity;
 
 architecture behavior of register_b_tb is
 
-    signal clk_tb      : std_logic := '0';
+	signal clk_tb      : std_logic := '0';
+	signal reset_tb    : std_logic := '0';
     signal data_in_tb  : std_logic_vector(31 downto 0) := (others => '0');
     signal data_out_tb : std_logic_vector(31 downto 0);
 
@@ -16,7 +17,8 @@ begin
 
     DUT: entity register_b
         port map (
-            clk      => clk_tb,
+			clk      => clk_tb,
+			reset	 => reset_tb,
             data_in  => data_in_tb,
             data_out => data_out_tb
         );
@@ -36,7 +38,16 @@ begin
     stim_proc: process
     begin
         wait for 20 ns;
-
+		
+		data_in_tb <= x"AAAA5555";
+		reset_tb <='1';
+        wait for clk_period;
+		reset_tb <='0';
+		
+		assert data_out_tb = x"00000000"
+        report "Error: Output mismatch after writing 00000000"
+        severity error;
+		
         data_in_tb <= x"AAAA5555";
         wait for clk_period;
 
--- design3/design0/src/register_b.vhd
+++ design3/design0/src/register_b.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity register_b is 
 	port (	clk		: in std_logic;
+			reset	: in std_logic;
 			data_in	: in std_logic_vector(31 downto 0);
 			data_out: out std_logic_vector(31 downto 0));
 end entity ;
@@ -16,7 +17,9 @@ begin
 	
 	process(clk)
 	begin
-		if rising_edge(clk) then
+		if reset='1' then
+			reg_b <= (others =>'0');
+		elsif rising_edge(clk) then
             reg_b <= data_in;
         end if;
     end process;
