35f85640b0d39fed7b84bada73d054e7b68ccd79
nadinee3 <167738397+nadinee3@users.noreply.github.com>
2025-05-06 23:49:08 +0300

    Added SLL and SRL

--- Code/ControlUnit_ALUControl.vhd
+++ Code/ControlUnit_ALUControl.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_1164.all;
 Entity ALUControl is 
 	port( 
 	-- inputs
-	instruc_15to0: IN std_ulogic_vector (15 downto 0);	
-	ALUOp:IN std_ulogic_vector (1 downto 0);
+	instruc_15to0: IN std_logic_vector (15 downto 0);	
+	ALUOp:IN std_logic_vector (1 downto 0);
 	-- outputs 
-	ALUopcode: OUT std_ulogic_vector (2 downto 0)
+	ALUopcode: OUT std_logic_vector (2 downto 0)
 	);
 End entity;	
 
@@ -15,12 +15,14 @@ Architecture Behavioral of ALUControl is
 begin 
 	process (instruc_15to0, ALUOp)	 
 	
-	--operations' function bits definition
-	Constant ADD_C: std_ulogic_vector (5 downto 0) := "100000";
-	Constant SUB_C: std_ulogic_vector (5 downto 0) := "100010";
-	Constant AND_C: std_ulogic_vector (5 downto 0) := "100100";
-	Constant OR_C: std_ulogic_vector (5 downto 0) := "100101";
-	Constant SLT_C: std_ulogic_vector (5 downto 0) := "101010";	  
+	--funct bits for different operations
+	Constant ADD_C: std_logic_vector (5 downto 0) := "100000";
+	Constant SUB_C: std_logic_vector (5 downto 0) := "100010";
+	Constant AND_C: std_logic_vector (5 downto 0) := "100100";
+	Constant OR_C: std_logic_vector (5 downto 0) := "100101";
+	Constant SLT_C: std_logic_vector (5 downto 0) := "101010";
+	Constant SLL_C: std_logic_vector (5 downto 0) := "000000";
+	Constant SRL_C: std_logic_vector (5 downto 0) := "000010";
 	
 	begin
 		case ALUop is 
@@ -32,7 +34,9 @@ begin
 				when SUB_C => ALUopcode <= "110"; --Subtraction (two registers)
 				when AND_C => ALUopcode <= "000"; --AND
 				when OR_C => ALUopcode <= "001"; --OR
-				when SLT_C => ALUopcode <= "111"; --SLT
+				when SLT_C => ALUopcode <= "111"; --SLT (set less than)
+				when SLL_C => ALUopcode <= "011"; --SLL	(shift left logical)
+				when SRL_C => ALUopcode <= "100"; --SRL (shift right logical)
 				when others => ALUopcode <= "000"; 
 			end case;
 			when others => ALUopcode <= "000"; 
